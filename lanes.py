7605e88fb6236023a6c7c665e7f5ddeaa22271c7
afozbek <furkanozbek1995@gmail.com>
2018-12-09 20:09:30 +0300

    some_outputs

--- lanes.py
+++ lanes.py
@@ -1,10 +1,29 @@
 import cv2
 import numpy as np
 # Read the image and turn into greyskale
+
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    canny = cv2.Canny(blur, 50, 150)
+    return canny
+
+
+def region_of_interest(image):
+    height = image.shape[0]
+    # We write a triangular which the machine can go!
+    polygons = np.array(
+        [[(199, height), (1099, height), (549, 249)]])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
-
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-
-cv2.imshow('result', gray)
+canny = canny(lane_image)
+cropped_image = region_of_interest(canny)
+cv2.imshow('result', cropped_image)
 cv2.waitKey(0)
