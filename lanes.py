81f37c383b4178279c35b1dbb5c025024cf2a6aa
afozbek <furkanozbek1995@gmail.com>
2018-12-09 20:42:59 +0300

    drawing_the_lines

--- lanes.py
+++ lanes.py
@@ -10,6 +10,16 @@ def canny(image):
     return canny
 
 
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            # we want to reshape the array to 1D
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return line_image
+
+
 def region_of_interest(image):
     height = image.shape[0]
     # We write a triangular which the machine can go!
@@ -25,5 +35,9 @@ image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
 cropped_image = region_of_interest(canny)
-cv2.imshow('result', cropped_image)
+lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
+                        np.array([]), minLineLength=40, maxLineGap=5)
+line_image = display_lines(lane_image, lines)
+combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+cv2.imshow('result', combo_image)
 cv2.waitKey(0)
