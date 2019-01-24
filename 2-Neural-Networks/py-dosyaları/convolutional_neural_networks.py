c04cc0fd8fdf87130c434a89f69b4be7d07ece45
afozbek <furkanozbek1995@gmail.com>
2019-01-24 22:05:24 +0300

    CNN_1_finished🤔🤢

--- 2-Neural-Networks/py-dosyaları/convolutional_neural_networks.py
+++ 2-Neural-Networks/py-dosyaları/convolutional_neural_networks.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import keras
 from keras.datasets import mnist
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Conv2D, MaxPooling3D
+from keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from keras.optimizers import Adam
 from keras.utils.np_utils import to_categorical
 import random
@@ -61,4 +61,65 @@ y_test = to_categorical(y_test, 10)
 
 X_train = X_train/255
 X_test = X_test/255
+
+#define the leNet_model method
+def leNet_model():
+  model = Sequential()
+  model.add(Conv2D(30, (5, 5),
+                   input_shape=(28, 28, 1),
+                  activation='relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Conv2D(15, (3, 3), activation='relu'))
+  model.add(MaxPooling2D(pool_size=(2, 2)))
+  model.add(Flatten())
+  model.add(Dense(500, activation='relu'))
+  model.add(Dense(num_classes, activation='softmax'))
+  model.compile(Adam(lr=0.01), 
+               loss='categorical_crossentropy',
+               metrics=['accuracy'])
+  return model
+
+model = leNet_model()
+print(model.summary())
+
+history = model.fit(X_train, y_train, epochs=10, 
+                    validation_split=0.1, 
+                    batch_size=400, verbose=1, shuffle=1)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['loss', 'val_loss'])
+plt.title('Loss')
+plt.xlabel('epoch')
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.legend(['acc', 'val_acc'])
+plt.title('Accuracy')
+plt.xlabel('epoch')
+
+import requests
+from PIL import Image
+
+#url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST8KzXHtkSHcxzdpnllMhAj0upLEwnNFdtY6j4YUPcmaf4Ty3u'
+url = 'https://www.researchgate.net/profile/Jose_Sempere/publication/221258631/figure/fig1/AS:305526891139075@1449854695342/Handwritten-digit-2.png'
+response = requests.get(url, stream = True)
+img = Image.open(response.raw)
+plt.imshow(img)
+
+import cv2
+
+img_array = np.asarray(img)
+resized = cv2.resize(img_array, (28, 28 ))
+gray_scale = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY) #(28, 28)
+image = cv2.bitwise_not(gray_scale)
+
+plt.imshow(image, cmap=plt.get_cmap('gray'))
+print(image)
+
+image = image / 255
+image = image.reshape(1, 28, 28, 1)
+
+prediction = model.predict_classes(image)
+print("predicted digit:", str(prediction))
 
