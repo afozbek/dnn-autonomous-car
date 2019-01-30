2d96761c605f26a15d0dfd740ca680927c45776c
afozbek <furkanozbek1995@gmail.com>
2019-01-30 12:52:03 +0300

    Preprocessing Images

--- 3-Building-Car-Model/py-dosyaları/behavioural_cloning.py
+++ 3-Building-Car-Model/py-dosyaları/behavioural_cloning.py
@@ -14,6 +14,7 @@ Original file is located at
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import keras
 from keras.models import Sequential
 from keras.optimizers import Adam
@@ -29,13 +30,16 @@ datadir = 'Track_Data'
 columns = ['center', 'left', 'right',
            'steering', 'throttle', 'reverse', 'speed']
 data = pd.read_csv(os.path.join(datadir, 'driving_log.csv'),
-             names=columns)
+                   names=columns)
 pd.set_option('display.max_colwidth', -1)
 data.head()
 
+
 def path_leaf(path):
-  head, tail = ntpath.split(path)
-  return tail
+    head, tail = ntpath.split(path)
+    return tail
+
+
 data['center'] = data['center'].apply(path_leaf)
 data['left'] = data['left'].apply(path_leaf)
 data['right'] = data['right'].apply(path_leaf)
@@ -45,48 +49,52 @@ num_bins = 25
 samples_per_bin = 200
 hist, bins = np.histogram(data['steering'], num_bins)
 center = (bins[:-1] + bins[1:]) * 0.5
-plt.bar(center, hist, width = 0.05)
+plt.bar(center, hist, width=0.05)
 plt.plot((np.min(data['steering']), np.max(data['steering'])),
-        (samples_per_bin, samples_per_bin))
+         (samples_per_bin, samples_per_bin))
 
 print('total data:', len(data))
 remove_list = []
 for j in range(num_bins):
-  list_ = []
-  for i in range(len(data['steering'])):
-    if data['steering'][i] >= bins[j] and data['steering'][i] <= bins[j + 1]:
-      list_.append(i)
-  list_ = shuffle(list_)
-  list_ = list_[samples_per_bin:]
-  remove_list.extend(list_)
+    list_ = []
+    for i in range(len(data['steering'])):
+        if data['steering'][i] >= bins[j] and data['steering'][i] <= bins[j + 1]:
+            list_.append(i)
+    list_ = shuffle(list_)
+    list_ = list_[samples_per_bin:]
+    remove_list.extend(list_)
 
 print('removed', len(remove_list))
 data.drop(data.index[remove_list], inplace=True)
 print('remaining', len(data))
 
 hist, _ = np.histogram(data['steering'], (num_bins))
-plt.bar(center, hist, width = 0.05)
+plt.bar(center, hist, width=0.05)
 plt.plot((np.min(data['steering']), np.max(data['steering'])),
-        (samples_per_bin, samples_per_bin))
+         (samples_per_bin, samples_per_bin))
 
-print(data.iloc[1]) #iloc-->records based on the indexes
+print(data.iloc[1])  # iloc-->records based on the indexes
+
+
 def load_img_steering(datadir, data):
-  image_path = []
-  steering = []
-  for i in range(len(data)):
-    indexed_data = data.iloc[i]
-    center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
-    image_path.append(os.path.join(datadir, center.strip()))
-    steering.append(float(indexed_data[3]))
-  #Convert the input to an array.  
-  image_paths = np.asarray(image_path) 
-  steerings = np.asarray(steering)
-  return image_paths, steerings
+    image_path = []
+    steering = []
+    for i in range(len(data)):
+        indexed_data = data.iloc[i]
+        center, left, right = indexed_data[0], indexed_data[1], indexed_data[2]
+        image_path.append(os.path.join(datadir, center.strip()))
+        steering.append(float(indexed_data[3]))
+    # Convert the input to an array.
+    image_paths = np.asarray(image_path)
+    steerings = np.asarray(steering)
+    return image_paths, steerings
+
 
 image_paths, steerings = load_img_steering(datadir + '/IMG', data)
 
-#Split arrays or matrices into random train and test subsets
-X_train, X_valid, y_train, y_valid = train_test_split(image_paths, steerings, test_size=0.2, random_state=6)
+# Split arrays or matrices into random train and test subsets
+X_train, X_valid, y_train, y_valid = train_test_split(
+    image_paths, steerings, test_size=0.2, random_state=6)
 
 print('Training Samples: {}\nValid Samples: {}'.format(len(X_train), len(X_valid)))
 
@@ -96,3 +104,35 @@ axes[0].set_title('Training set')
 axes[1].hist(y_valid, bins=num_bins, width=0.05, color='red')
 axes[1].set_title('Valid set')
 
+
+def img_preprocess(img_path):
+    img = mpimg.imread(img_path)
+    img = img[60:136, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
+    return img
+
+
+image_path = image_paths[60]
+original_image = mpimg.imread(image_path)
+preprocessed_image = img_preprocess(image_path)
+
+fig, axes = plt.subplots(1, 2, figsize=(15, 10))
+fig.tight_layout()
+axes[0].imshow(original_image)
+axes[0].set_title('Original Image')
+axes[1].imshow(preprocessed_image)
+axes[1].set_title('Preprocessed Image')
+
+# Make an iterator that computes the function
+# using arguments from each of the iterables.
+# Each image in X_traing goes as parameter
+X_train = np.array(list(map(img_preprocess, X_train)))
+# Each image in X_traing goes as parameter
+X_valid = np.array(list(map(img_preprocess, X_valid)))
+
+plt.imshow(X_train[random.randint(0, len(X_train) - 1)])
+plt.axis("off")
+print(X_train.shape)
