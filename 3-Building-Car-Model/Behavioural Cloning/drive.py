37980141ce5953a6910d0dd59485a3f8dcf1622c
afozbek <furkanozbek1995@gmail.com>
2019-01-30 18:12:57 +0300

    Self Driving Car -  Test - 1

--- 3-Building-Car-Model/Behavioural Cloning/drive.py
+++ 3-Building-Car-Model/Behavioural Cloning/drive.py
@@ -1,10 +1,40 @@
 import socketio
 import eventlet
+import numpy as np
 from flask import Flask
+from keras.models import load_model
+import base64
+from io import BytesIO
+from PIL import Image
+import cv2
 
 sio = socketio.Server()
 
 app = Flask(__name__)  # '__main__'
+
+speed_limit = 10
+
+
+def img_preprocess(img_path):
+    img = img_path[60:136, :, :]
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
+    return img
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    speed = float(data['speed'])
+    image = Image.open(BytesIO(base64.b64decode(data['image'])))
+    image = np.asarray(image)
+    image = img_preprocess(image)
+    image = np.array([image])
+    steering_angle = float(model.predict(image))
+    throttle = 1.0 - speed / speed_limit
+    print('{} {} {}'.format(steering_angle, throttle, speed))
+    send_control(steering_angle, throttle)
 
 
 @sio.on('connect')  # message, disconnect
@@ -12,12 +42,16 @@ def connect(sid, environ):
     print('Connected')
     send_control(0, 1)
 
+
 def send_control(steering_angle, throttle):
-    #Event name in the udacity simulator
-    sio.emit('steer', data = {
+    # Event name in the udacity simulator
+    sio.emit('steer', data={
         'steering_angle': steering_angle.__str__(),
         'throttle': throttle.__str__()
     })
+
+
 if __name__ == "__main__":
+    model = load_model('model.h5')
     app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
